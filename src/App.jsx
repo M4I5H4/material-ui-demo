6436b753599e26fe96156ea0e9e82505f62bc2df
M4I5HA <maisha.adamsam@gmail.com>
2022-03-04 16:02:59 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Typography } from "@material-ui/core"; 
+
+function App()  {
+
+    return(
+    <div>
+        <Typography variant="h1">HELLO, WORLD!</Typography>
+    </div>
+    );
+};
+
+export default App;
