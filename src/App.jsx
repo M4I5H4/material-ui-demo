fd0c0b1a692b3cfd9239d88b0351803d133e0780
M4I5HA <maisha.adamsam@gmail.com>
2022-03-04 17:32:23 +0000

    added styling to buttons and headers

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
 import React from "react";
-import { Typography } from "@material-ui/core"; 
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button} from "@material-ui/core"; 
+import { PhotoCamera } from '@material-ui/icons';
+import useStyles from "./styles";
+
+//create a hook called useStyles which is equal to a function call for makeStyles and this utilises a call back fuction which returns an object which contains all the styling
+
+
 
 function App()  {
-
+    const classes = useStyles();
     return(
-    <div>
-        <Typography variant="h1">HELLO, WORLD!</Typography>
-    </div>
+    <>
+        <CssBaseline />
+        <AppBar position="relative">
+            <Toolbar>
+                <PhotoCamera className={classes.icon}/>
+                <Typography variant="h6">
+                    Photo Album
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        <main>
+            <div className={classes.container}>
+                <Container maxWidth="sm" >
+                    <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                        Photo Album
+                    </Typography>
+                    <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                        Hello everyone this is a photo album and I'm trying to make this sentence long so we can see how it looks like on the screen.
+                    </Typography>
+                    <div className={classes.buttons}>
+                        <Grid container spacing={2} justify="center">
+                            <Grid item>
+                                <Button variant="contained" color="primary">
+                                    See my Photos
+                                </Button>
+                            </Grid>
+                            <Grid item>
+                                <Button variant="outlined" color="primary">
+                                    Secondary Action
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </div>
+                </Container>
+            </div>
+
+        </main>
+    </>
     );
 };
 
