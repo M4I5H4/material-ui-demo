41e0249ed41686a9904781b4b6a97a823779c5d5
M4I5HA <maisha.adamsam@gmail.com>
2022-03-04 18:07:03 +0000

    created cards

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,28 @@ function App()  {
                     </div>
                 </Container>
             </div>
-
+            <Container className={classes.cardGrid} maxWidth="md">
+                <Grid container spacing={4}>
+                    <Grid item>
+                        <Card className={classes.card}>
+                            <CardMedia className={classes.cardMedia} image="https://source.unsplash.com/random" 
+                            title="Image Title"/>
+                            <CardContent className={classes.cardContent}>
+                                <Typography gutterBottom variant="h5">
+                                    Heading
+                                </Typography>
+                                <Typography>
+                                    This is a media card. You can use this section to desc the content
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" color="primary">View</Button>
+                                <Button size="small" color="primary">Edit</Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                </Grid>
+            </Container>
         </main>
     </>
     );
--- src/styles.js
+++ src/styles.js
@@ -1,15 +1,31 @@
-import { makeStyles } from "@material-ui/core/styles";
-const useStyles = makeStyles((theme) =>({
-    container:{
-        backgroundColor: theme.palette.background.paper,
-        padding: theme.spacing(8, 0, 6)
-    },
-    icon:{
-        marginRight: '20px'
-    },
-    buttons:{
-        marginTop: '40px'
-    }
+import { CardMedia } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles((theme) => ({
+  container: {
+    backgroundColor:
+      theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  icon: {
+    marginRight: '20px',
+  },
+  buttons: {
+    marginTop: '40px',
+  },
+  cardGrid: {
+    padding: '20px 0',
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.23%', //gives a 16:9 ratio
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
 }));
 
 export default useStyles;
